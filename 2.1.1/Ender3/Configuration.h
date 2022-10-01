5722216b3b9d3d476d84654daf6db9f5c87f69bc
mrmazakblu <mrmazakblu@gmail.com>
2022-10-01 15:51:42 -0400

    update build_
    add config output
    increase Z travel for auto square move

--- 2.1.1/Ender3/Configuration.h
+++ 2.1.1/Ender3/Configuration.h
@@ -1697,11 +1697,11 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -100
+#define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 240
+#define Z_MAX_POS 242
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
